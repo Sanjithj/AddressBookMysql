fb75128eee82ecb281607f1114c995fa1d2cb5cf
Sanjithj <sanjithj591997@gmail.com>
2022-07-30 11:34:52 +0530

    Added: Create a database

--- Mysql.java
+++ Mysql.java
@@ -3,6 +3,10 @@ package com.bridgelabz;
 public class Mysql {
 	public static void main(String[] args) {
 		System.out.println("Welcome to AddressBook in DataBase");
+		
+		 create database Address_Book_Service;
+		 use Address_Book_Service;
+		 show databases;
 	}
 
 }
